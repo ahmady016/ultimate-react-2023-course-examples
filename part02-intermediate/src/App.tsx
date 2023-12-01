4bd957fc38ddd95577a262bd8bf24f6f798abd52
ahmady016 <ahmady09@gmail.com>
2023-12-01 22:07:22 +0200

    part02: install and test tailwindcss

--- part02-intermediate/src/App.tsx
+++ part02-intermediate/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const App: React.FC = () => (
-    <h1>Ultimate React Course 2023 Part 02 Intermediate</h1>
+    <h1 className="font-bold text-center text-2xl">
+        Ultimate React Course 2023 Part 02 Intermediate
+    </h1>
 )
 
 export default App
