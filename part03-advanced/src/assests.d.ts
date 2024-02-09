e90148d4bc3c0d8dc17516fd2ce3235234657cfc
ahmady016 <ahmady09@gmail.com>
2024-02-09 20:26:05 +0200

    part30: allow importing [m4a] sound files

---
+++ part03-advanced/src/assests.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.mp3' {
+	const value: any
+	export default value
+}
+declare module '*.m4a' {
+	const value: any
+	export default value
+}
