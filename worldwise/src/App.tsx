8e3d268c346a154b097b520b94512ab0569df180
ahmady016 <ahmady09@gmail.com>
2024-03-12 06:11:10 +0200

    part03 - world-wise: Optimize App with React Lazy and Suspense

--- worldwise/src/App.tsx
+++ worldwise/src/App.tsx
@@ -1,48 +1,51 @@
+import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import { FakeAuthProvider } from './components/FakeAuthContext'
 import { CitiesProvider } from './app/CitiesContext'
 
-import HomeLayout from './home/components/HomeLayout'
-import HomePage from './home/components/HomePage'
-import ProductPage from './home/components/ProductPage'
-import LoginPage from './home/components/LoginPage'
-import PricingPage from './home/components/PricingPage'
-
-import AppLayout from './app/components/AppLayout'
+import AuthedRoute from './components/AuthedRoute'
+import Spinner from './components/Spinner'
 import Cities from './app/components/Cities'
 import CityDetails from './app/components/CityDetails'
 import Countries from './app/components/Countries'
 import AddCityForm from './app/components/AddCityForm'
 
-import NotFoundPage from './NotFoundPage'
-import AuthedRoute from './components/AuthedRoute'
+const HomeLayout = React.lazy(() => import('./home/components/HomeLayout'))
+const HomePage = React.lazy(() => import('./home/components/HomePage'))
+const ProductPage = React.lazy(() => import('./home/components/ProductPage'))
+const LoginPage = React.lazy(() => import('./home/components/LoginPage'))
+const PricingPage = React.lazy(() => import('./home/components/PricingPage'))
+const AppLayout = React.lazy(() => import('./app/components/AppLayout'))
+const NotFoundPage = React.lazy(() => import('./NotFoundPage'))
 
 const App: React.FC = () => {
 	return (
 		<FakeAuthProvider>
 			<CitiesProvider>
 				<BrowserRouter>
-					<Routes>
-						<Route element={<HomeLayout />}>
-							<Route index element={<HomePage />} />
-							<Route path="product" element={<ProductPage />} />
-							<Route path="pricing" element={<PricingPage />} />
-							<Route path="login" element={<LoginPage />} />
-						</Route>
-						<Route path="app" element={
-							<AuthedRoute>
-								<AppLayout />
-							</AuthedRoute>
-						}>
-							<Route index element={<Navigate replace to="cities" />} />
-							<Route path="cities" element={<Cities />} />
-							<Route path="cities/:id" element={<CityDetails />} />
-							<Route path="countries" element={<Countries />} />
-							<Route path="add-city" element={<AddCityForm />} />
-						</Route>
-						<Route path="*" element={<NotFoundPage />} />
-					</Routes>
+					<React.Suspense fallback={<Spinner fullScreen />}>
+						<Routes>
+							<Route element={<HomeLayout />}>
+								<Route index element={<HomePage />} />
+								<Route path="product" element={<ProductPage />} />
+								<Route path="pricing" element={<PricingPage />} />
+								<Route path="login" element={<LoginPage />} />
+							</Route>
+							<Route path="app" element={
+								<AuthedRoute>
+									<AppLayout />
+								</AuthedRoute>
+							}>
+								<Route index element={<Navigate replace to="cities" />} />
+								<Route path="cities" element={<Cities />} />
+								<Route path="cities/:id" element={<CityDetails />} />
+								<Route path="countries" element={<Countries />} />
+								<Route path="add-city" element={<AddCityForm />} />
+							</Route>
+							<Route path="*" element={<NotFoundPage />} />
+						</Routes>
+					</React.Suspense>
 				</BrowserRouter>
 			</CitiesProvider>
 		</FakeAuthProvider>
