4560587e41df830cc814eed684e7e943613dbbbe
ahmady016 <ahmady09@gmail.com>
2024-05-09 05:36:41 +0300

    part04->fast-pizza: add prettier tailwind plugin

--- part04-professional/fast-pizza/src/layouts/HomePage.tsx
+++ part04-professional/fast-pizza/src/layouts/HomePage.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 
 const HomePage: React.FC = () => {
-	return (
-		<div className="my-10 px-4 text-center sm:my-16">
-			<h1 className="mb-8 text-xl font-semibold md:text-3xl">
-				<p className="mb-4">The Best Pizza.</p>
-				<span className="text-yellow-500">
-					Straight out of the oven, straight to you.
-				</span>
-			</h1>
-		</div>
-	)
+    return (
+        <div className="my-10 px-4 text-center sm:my-16">
+            <h1 className="mb-8 text-xl font-semibold md:text-3xl">
+                <p className="mb-4">The Best Pizza.</p>
+                <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
+            </h1>
+        </div>
+    )
 }
 
 export default HomePage
