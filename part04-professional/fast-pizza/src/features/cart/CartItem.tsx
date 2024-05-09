bdd935187fc940b33d9c81ed2b77a5cfe040e672
ahmady016 <ahmady09@gmail.com>
2024-05-09 18:11:34 +0300

    part04->fast-pizza: styling the Cart

--- part04-professional/fast-pizza/src/features/cart/CartItem.tsx
+++ part04-professional/fast-pizza/src/features/cart/CartItem.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { IoCloseOutline } from 'react-icons/io5'
 
-const CartItem: React.FC = () => {
-	return <div>CartItem</div>
+import { Cart } from '../../services/apiRestaurant'
+import { formatCurrency } from '../../services/helpers'
+
+import Button from '../../components/Button'
+
+const CartItem: React.FC<Cart> = ({ pizzaId, name, quantity, unitPrice }) => {
+	return (
+		<li id={pizzaId.toString()} className="py-3 sm:flex sm:items-center sm:justify-between">
+			<p className="mb-1 flex items-center sm:mb-0">
+				{quantity} <IoCloseOutline className="mx-2 sm:mx-1" /> {name}
+			</p>
+			<div className="flex items-center justify-between sm:gap-6">
+				<p>{formatCurrency(unitPrice)}</p>
+				<Button variant="small">DELETE</Button>
+			</div>
+		</li>
+	)
 }
 
 export default CartItem
--- part04-professional/fast-pizza/src/features/cart/CartPage.tsx
+++ part04-professional/fast-pizza/src/features/cart/CartPage.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import { BiArrowBack } from 'react-icons/bi'
+
+import { initialCart } from '../../services/apiRestaurant'
+
+import CartItem from './CartItem'
+import LinkButton from '../../components/LinkButton'
+import Button from '../../components/Button'
 
 const CartPage: React.FC = () => {
 	return (
-		<div className="my-10 px-4 text-center sm:my-16">
-			<h1 className="mb-8 text-xl font-semibold md:text-3xl">
-				<p className="mb-4">The Cart Page.</p>
-				<span className="text-yellow-500">
-					Page to display Cart items and status.
-				</span>
-			</h1>
+		<div className="text-center sm:my-16 lg:my-4">
+			<LinkButton to="/pizza-menu" className="text-md flex w-32 items-center justify-evenly font-medium scale-125">
+				<BiArrowBack />
+				<span>Back To Menu</span>
+			</LinkButton>
+			<h2 className="mt-6 text-xl font-semibold md:text-2xl">Your Cart %NAME%</h2>
+			<ul className="mt-3 divide-y divide-stone-200 border-b">
+				{initialCart.map(item => <CartItem key={item.pizzaId} {...item} /> )}
+			</ul>
+			<div className="mt-6 space-x-2">
+				<Button to="/order/new" variant="primary">Order Pizzas</Button>
+				<Button variant="secondary">Clear Cart</Button>
+			</div>
 		</div>
 	)
 }
