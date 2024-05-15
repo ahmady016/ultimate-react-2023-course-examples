64305455a2aa3732a85acffb5d6edd5283bbcf90
ahmady016 <ahmady09@gmail.com>
2024-05-15 16:31:43 +0300

    part04->fast-pizza: Refactor some styles

--- part04-professional/fast-pizza/src/features/cart/CartStatusBar.tsx
+++ part04-professional/fast-pizza/src/features/cart/CartStatusBar.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+
+import { BsCart3 } from 'react-icons/bs'
 import { FaLongArrowAltRight } from 'react-icons/fa'
 
 import { formatCurrency } from '../../services/helpers'
@@ -14,12 +16,13 @@ const CartStatusBar: React.FC = () => {
 	if (isEmptyCart) return null
 
 	return (
-		<div className="flex items-center justify-between bg-stone-300 px-4 py-4 text-sm uppercase text-stone-700 sm:px-6 md:text-base">
-			<p className="space-x-4 font-semibold text-stone-600 sm:space-x-6">
+		<div className="flex items-center justify-between bg-stone-700 px-4 py-2 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+			<p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
 				<span>{totalQuantity} pizzas</span>
 				<span>{formatCurrency(totalPrice)}</span>
 			</p>
 			<Link to="/cart" className="flex items-center gap-2 font-semibold">
+				<BsCart3 className="text-lg" />
 				<span>Open Cart</span>
 				<FaLongArrowAltRight />
 			</Link>
--- part04-professional/fast-pizza/src/layouts/Footer.tsx
+++ part04-professional/fast-pizza/src/layouts/Footer.tsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Footer: React.FC = () => {
 	return (
-		<footer className="bg-stone-700 shadow">
-			<div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
-				<p className="text-md text-center text-stone-200">
+		<footer className="bg-stone-300 shadow">
+			<div className="w-full mx-auto max-w-screen-xl py-3 flex items-center justify-center">
+				<p className="text-md text-center text-stone-700">
 					© 2024 &nbsp;
 					<Link to="/" className="text-bold hover:underline">Fast Pizza Co™</Link>
 					&nbsp;. All Rights Reserved.
