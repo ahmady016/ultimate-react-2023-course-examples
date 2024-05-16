514aeaa8570f352243376e9ab00e822e44482c0d
ahmady016 <ahmady09@gmail.com>
2024-05-16 17:35:20 +0300

    part04->fast-pizza: Fetch PizzaMenuItems in OrderPage using routerFetcher

--- part04-professional/fast-pizza/src/features/order/OrderItem.tsx
+++ part04-professional/fast-pizza/src/features/order/OrderItem.tsx
@@ -4,15 +4,29 @@ import { IoCloseOutline } from 'react-icons/io5'
 import { formatCurrency } from '../../services/helpers'
 import { Cart } from '../../services/apiRestaurant'
 
-const OrderItem: React.FC<Cart> = ({ pizzaId, name, quantity, totalPrice }) => {
+type OrderItemProps = Cart & {
+	isLoadingIngredients: boolean
+	ingredients: string[]
+}
+const OrderItem: React.FC<OrderItemProps> = ({
+	pizzaId,
+	name,
+	quantity,
+	totalPrice,
+	isLoadingIngredients,
+	ingredients,
+}) => {
 	return (
-		<li id={pizzaId.toString()} className="py-2 px-3">
+		<li id={pizzaId.toString()} className="space-y-2 p-3">
 			<div className="flex items-center justify-between gap-4 text-[0.9rem]">
 				<p className="mb-2 flex items-center font-bold sm:mb-0">
 					{quantity} <IoCloseOutline className="mx-2 sm:mx-1" /> {name}
 				</p>
 				<p className="font-bold">{formatCurrency(totalPrice)}</p>
 			</div>
+			<p className="text-sm capitalize italic text-stone-500">
+				{isLoadingIngredients ? 'Loading Ingredients ...' : ingredients.join(', ')}
+			</p>
 		</li>
 	)
 }
--- part04-professional/fast-pizza/src/features/order/OrderPage.tsx
+++ part04-professional/fast-pizza/src/features/order/OrderPage.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Params, useLoaderData } from 'react-router-dom'
+import { Params, useFetcher, useLoaderData } from 'react-router-dom'
 import { AxiosResponse } from 'axios'
 
+import { getOrder, Order, Pizza } from '../../services/apiRestaurant'
 import { calcMinutesLeft, formatCurrency, formatDate } from '../../services/helpers'
-import { getOrder, Order } from '../../services/apiRestaurant'
 
 import OrderItem from './OrderItem'
 
 const OrderPage: React.FC = () => {
 	const { data: order } = useLoaderData() as AxiosResponse<Order>
 	const deliveryIn = calcMinutesLeft(order.estimatedDelivery)
+
+	const routerFetcher = useFetcher()
+	React.useEffect(() => {
+		if(routerFetcher.state === 'idle' && !routerFetcher.data)
+			routerFetcher.load('/pizza-menu')
+	}, [routerFetcher])
 
 	return (
 		<div className="space-y-5 px-4 py-6">
@@ -37,7 +43,14 @@ const OrderPage: React.FC = () => {
 				<p className="text-sm text-stone-600">(Estimated delivery: {formatDate(order.estimatedDelivery)})</p>
 			</div>
 			<ul className="divide-y divide-stone-200 border-b border-t py-2">
-				{order.cart.map(item => <OrderItem key={item.pizzaId} {...item} />)}
+				{order.cart.map(item =>
+					<OrderItem
+						key={item.pizzaId}
+						isLoadingIngredients={routerFetcher.state === 'loading'}
+						ingredients={routerFetcher?.data?.data?.find((pizzaItem: Pizza) => pizzaItem.id === item.pizzaId)?.ingredients ||[]}
+						{...item}
+					/>
+				)}
 			</ul>
 			<div className="space-y-2 bg-stone-200 px-6 py-5">
 				<p className="text-sm font-medium text-stone-600">
